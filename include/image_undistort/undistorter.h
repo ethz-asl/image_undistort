615876d1938a0f71349acc42ac2d2e4f428466b9
smb <arnold@skynet>
2021-04-19 14:10:11 +0200

    Fixes for noetic compilation

--- include/image_undistort/undistorter.h
+++ include/image_undistort/undistorter.h
@@ -3,8 +3,8 @@
 
 #include <Eigen/Dense>
 
-#include <cv.h>
-#include <highgui.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include "image_undistort/camera_parameters.h"
